1325340afc7a76440597bcc28c6bafd417257592
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-25 15:52:38 +0600

    all price added

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
-    return (
-        <div>
+    const {cart} = props;
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+        }
+        // const tax = (total * 0.1).toFixed(2); //1% tax dewa lagbe proti product er jonno //toFixed use korar karone aita string hoice pore use er jonno number a convert kora lagbe
+        // const grandTotal = total + Number(tax) + shipping; //number a convert korte Number, parseFloat, + use hoy
+        const tax = parseFloat((total * 0.1).toFixed(2));
+        const grandTotal = total + tax + shipping; //option 2
+        return (
+        <div className='cart'>
              <h4>Order summery</h4>
-                <p>selected items: {props.cart.length}</p>
-                <p>{console.log(props.cart)}</p>
+             <p>{console.log(cart)}</p>
+                <p>Total Price: $ {total}</p>
+                <p>Total Shipping: $ {shipping}</p>
+                <p>Tax: {tax}</p>
+                <p>Grand Total: {grandTotal.toFixed(2)}</p>
+            
         </div>
     );
 };
 
-export default Cart;
+export default Cart; 
