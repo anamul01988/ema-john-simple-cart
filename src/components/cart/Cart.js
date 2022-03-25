165b9c5c6c786a0e9c457f55b7dfde7df7121194
Anamul haque  shanto <anamulhaque0198877@gmail.com>
2022-03-25 22:49:58 +0600

    data in order summery stored in local storage

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,12 +1,16 @@
+import { queryAllByAltText } from '@testing-library/react';
 import React from 'react';
 import './Cart.css';
 const Cart = (props) => {
     const {cart} = props;
+    // console.log(cart)
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for(const product of cart){
-        total = total + product.price;
-        shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping * product.shipping;
         }
         // const tax = (total * 0.1).toFixed(2); //1% tax dewa lagbe proti product er jonno //toFixed use korar karone aita string hoice pore use er jonno number a convert kora lagbe
         // const grandTotal = total + Number(tax) + shipping; //number a convert korte Number, parseFloat, + use hoy
@@ -15,7 +19,9 @@ const Cart = (props) => {
         return (
         <div className='cart'>
              <h4>Order summery</h4>
-             <p>{console.log(cart)}</p>
+            {/* <p>Selected items : {cart.length}</p>  */}
+            <p>Selected items : {quantity}</p> 
+             {/* <p>{console.log(cart)}</p> */}
                 <p>Total Price: $ {total}</p>
                 <p>Total Shipping: $ {shipping}</p>
                 <p>Tax: {tax}</p>
--- src/components/shop/Shop.js
+++ src/components/shop/Shop.js
@@ -1,21 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../cart/Cart';
 import Product from '../product/Product';
+import { addToDb, getStoredCart } from '../../utilities/fakedb'; //aita import hoite jhamela kore
 import './Shop.css';
 const Shop = () => {
     const [products, setProducts]= useState([]);
     const [cart,setCart] = useState([]);
     useEffect(()=>{
+        console.log("product load before fetch")
        fetch('products.json')
        .then(res =>res.json())
-       .then(data =>setProducts(data))
+       .then(data =>{
+        setProducts(data)
+        console.log('products loaded');
+       })
     },[]);
 
-    const handleAddToCart = (product)=>{
-        console.log(product)
-        // aita use e korbo nah --> cart.push(product) -->age aita use hoito ekhn spread operator use hoy ager value gula store korte
-        const newCart = [...cart, product]; //ager gula copy kore arry er moddhe boshbabo then notun gula  aisha boshbe
+    useEffect(()=>{
+        console.log("locla storage first line", products)
+        const storedCart = getStoredCart();
+        const savedCart = [];
+        console.log(storedCart);
+        for(const id in storedCart){
+            // console.log(id)
+            const addedProduct = products.find(product =>product.id === id);
+             if(addedProduct){
+                 const quantity = storedCart[id];
+                 addedProduct.quantity = quantity;  //addedProduct er keta property hocce quantity tar moddhe quantiy value rakhteci jeitga dynamically pacci joto bar product press korbo totbar count hobe
+                //   console.log(addedProduct)
+                savedCart.push(addedProduct);
+             }
+        }
+        // console.log('local storage finished')
+        setCart(savedCart);
+    },[products])
+
+
+    const handleAddToCart = (selectedProdcut)=>{
+        console.log(selectedProdcut)
+        let newCart = [];
+        const exists = cart.find(product => product.id === selectedProdcut.id);
+        if(!exists){
+            selectedProdcut.quantity = 1;
+            newCart = [...cart, selectedProdcut];
+        }
+        else{
+            const rest = cart.filter(product =>product.id !== selectedProdcut.id);
+            exists.quantity = exists.quantity + 1;
+            newCart = [...rest, exists];
+        }
+   
         setCart(newCart);
+        addToDb(selectedProdcut.id);
+
    }
     return (
         <div className='shop-container'>
